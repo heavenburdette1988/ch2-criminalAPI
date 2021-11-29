79222466c2c78788733600e933ed072c849c49dd
heavenburdette1988 <heavenburdette1988@gmail.com>
2021-11-29 13:59:41 -0500

    facilities extra practice complete

--- src/scripts/Locations/location.js
+++ src/scripts/Locations/location.js
@@ -1,9 +1,22 @@
-export const locations = (Object) => {
+
+
+
+
+
+
+export const locations = (locationObject, criminals ) => {
     return `
     <section class="locationCard">
-    <h3 class="id"><a>ID: </a>${Object.id}</h3>
-    <div class="facilityName"><a>Facility: </a>${Object.facilityName}</div>
-    <div class="SecuityLevel"><a>SecuirityLevel: </a>${Object.securityLevel}</div>
-    <div class="Capacity"><a>Capacity: </a>${Object.capacity}</div>
+    <div>
+    <h3 class="facility">Facility: ${locationObject.facilityName}</h3>
+    
+    <div class="SecuityLevel"><a>SecuirityLevel: </a>${locationObject.securityLevel}</div>
+    <div class="Capacity"><a>Capacity: </a>${locationObject.capacity}</div>
+    </div>
+    <h3>Criminals</h3>
+    <ul>
+    ${criminals.map(c => `<li>${c.name}</li>`).join("")}
+    </ul>
+  
     </section>`
 }
--- src/scripts/Locations/locationList.js
+++ src/scripts/Locations/locationList.js
@@ -1,24 +1,64 @@
 import { useLocations, getLocations } from "./locationDateProvider.js";
 import { locations } from "./location.js";
+import { getCriminals, useCriminals } from "../CriminalsFolder/CriminalsDataProvider.js";
+import { getCriminalFacilities, useCriminalFacilities } from "../CriminalsFolder/CriminalFacilityProvider.js";
 
 const contentTarget = document.querySelector(".contentContainer")
 
-export const LocationsList = () => {
-    getLocations()
-    .then(() => {
-        let locationArray = useLocations();
+// export const LocationsList = () => {
+//     getLocations()
+//     .then(() => {
+//         let locationArray = useLocations();
 
-        let locHTML = "";
+//         let locHTML = "";
 
-        locationArray.forEach((singleLocObj) => {
-                locHTML += locations(singleLocObj);
-    })
-    contentTarget.innerHTML = `<h2 class="locationHeader">Facilities</h2><div class="styledContainer">${locHTML}</div>`
-    })
-}
+//         locationArray.forEach((singleLocObj) => {
+//                 locHTML += locations(singleLocObj);
+//     })
+//     contentTarget.innerHTML = `<h2 class="locationHeader">Facilities</h2><div class="styledContainer">${locHTML}</div>`
+//     })
+// }
 
 
 document.querySelector("#facilitiesnav-link").addEventListener("click", () => {
     LocationsList()
     })
+    
+
+    export const LocationsList = () => {
+        // Kick off the fetching of both collections of data
+        getCriminals()
+            .then(getLocations)
+            .then(getCriminalFacilities)
+            .then(
+                () => {
+                    // Pull in the data now that it has been fetched
+                    const facilities = useLocations()
+                    const crimFac = useCriminalFacilities()
+                    const criminals = useCriminals()
+    
+                    // Pass all three collections of data to render()
+                    render(facilities, criminals, crimFac)
+                }
+            )
+    }
+    
+    const render = (locationsToRender, allCriminals, allRelationships) => {
+        // Step 1 - Iterate all criminals
+        contentTarget.innerHTML = locationsToRender.map(
+            (locationObject) => {
+                // Step 2 - Filter all relationships to get only ones for this criminal
+                const criminalsRelationshipsForThisLocation = allRelationships.filter(cf => cf.facilityId === locationObject.id)
+    
+                // Step 3 - Convert the relationships to facilities with map()
+                const criminals = criminalsRelationshipsForThisLocation.map(cf => {
+                    const matchingCriminalObject = allCriminals.find(criminal => criminal.id === cf.criminalId)
+                    return matchingCriminalObject
+                })
+    
+                // Must pass the matching facilities to the Criminal component
+                return locations(locationObject, criminals)
+            }
+        ).join("")
+    }
     
